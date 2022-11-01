a7ec29ce497c105026fc60e0511e42e341bc758e
wallacermr <wallacermr@gmail.com>
2022-10-31 23:49:14 -0300

    first commit

---
+++ src/main/java/br/com/wallace/rafael/snippets/SnippetsApplication.java
@@ -0,0 +1,13 @@
+package br.com.wallace.rafael.snippets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnippetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnippetsApplication.class, args);
+	}
+
+}
