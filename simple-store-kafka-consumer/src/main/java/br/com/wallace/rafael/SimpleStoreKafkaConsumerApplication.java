b5ff8957523205206affaa293ded149f00c39f67
wallacermr <wallacermr@gmail.com>
2022-11-06 07:09:36 -0300

    create consumer

---
+++ simple-store-kafka-consumer/src/main/java/br/com/wallace/rafael/SimpleStoreKafkaConsumerApplication.java
@@ -0,0 +1,11 @@
+package br.com.wallace.rafael;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleStoreKafkaConsumerApplication {
+    public static void main( String[] args ) {
+        SpringApplication.run(SimpleStoreKafkaConsumerApplication.class, args);
+    }
+}
---
+++ simple-store-kafka-consumer/src/test/java/br/com/wallace/rafael/SimpleStoreKafkaConsumerApplicationTest.java
@@ -0,0 +1,14 @@
+package br.com.wallace.rafael;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SimpleStoreKafkaConsumerApplicationTest {
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
