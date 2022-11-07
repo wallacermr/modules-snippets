ac83323a0d886efa2f82d0ea5415918adaeb0ad4
wallacermr <wallacermr@gmail.com>
2022-11-07 17:51:46 -0300

    changes

---
+++ simple-store-kafka-consumer/src/test/java/br/com/wallace/rafael/SimpleStoreKafkaConsumerApplicationTest.java
@@ -0,0 +1,13 @@
+package br.com.wallace.rafael;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SimpleStoreKafkaConsumerApplicationTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+}
